13e20e2f6dad42ccba35f5fa386baa55a5b150f1
paratellaDev <paratelladevmarcos@gmail.com>
2024-06-14 11:27:52 -0300

    removendo '' de PORT

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const PORT = (3000);
+const PORT = 3000;
 
-app.listen('PORT', () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 });
+
