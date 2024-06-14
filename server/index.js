afc24aaeade2c5b19d9c24ac0707f392c770735d
paratellaDev <paratelladevmarcos@gmail.com>
2024-06-14 12:38:57 -0300

    criação de model criando as tabelas

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3001;
+const db = require('./models');
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-});
-
+// db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+// });
