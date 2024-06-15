19cf8a47b3de4d88ecf5ed8ca7b557a3fdee2aef
paratellaDev <paratelladevmarcos@gmail.com>
2024-06-15 11:30:19 -0300

    alteração dos metodos

--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -1,21 +1,23 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { Posts } = require("../models/");
+const { Post } = require('../models');
 
-router.get("/", (req, res) => {
-  res.json("Server is running on");
+console.log('Modelo Post:', Post);
+
+router.post('/', async (req, res) => {
+  const post = req.body;
+  console.log('Recebido', post);
+  try {
+    const createdPost = await Post.create(post);
+    res.json(createdPost);
+  } catch (error) {
+    console.error('Erro ao criar o post:', error);
+    res.status(500).json({ error: 'Erro ao criar o post' });
+  }
 });
 
-router.post("/", async (req, res) => {
-  const post = req.body;
-  try {
-    console.log("Recebido",req.body);
-    const cretedPost = await Posts.create(post);
-    res.json(cretedPost);
-  } catch (error) {
-    console.error("Erro ao criar o post",error);
-    res.status(500).json({ error: "Erro ao tentar criar o Post" });
-  }
+router.get("/", (req, res) => {
+  res.json("Server is running on port 3001");
 });
 
 module.exports = router;
