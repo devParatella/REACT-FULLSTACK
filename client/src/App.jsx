87137b668b44283bfe280fdd52fe9ac20e7479b4
devparatellamarcos@gmail.com <devparatellamarcos@gmail.com>
2024-06-17 14:12:14 -0300

    iniciando as configurações do Axios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
-import './App.css'
+import "./App.css";
+import axios from "axios";
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/Posts")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar posts", error);
+      });
+  }, []);
 
   return (
-    <div className="App">
-
-    </div>
-  )
-export default App
+  <div className="App">
+    {/* Lógica para renderizar os posts */}
+  </div>
+  );
+}
+export default App;
